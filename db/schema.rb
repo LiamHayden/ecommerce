4d4fc8cb63497b1d448b202606b4273cca600314
Liam Hayden <l.hayden67@outlook.com>
2023-03-09 16:46:12 +0000

    Add Item model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_163529) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_164512) do
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.float "price"
+    t.boolean "inStock"
+    t.integer "units"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "stores", force: :cascade do |t|
     t.string "type"
     t.datetime "created_at", null: false
